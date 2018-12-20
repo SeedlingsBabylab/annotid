baf0149bc6f2f309949df15160d9831683bbfd53
Gladys Baudet <gladys.baudet@gmail.com>
2018-12-20 11:27:11 -0500

    add windows path

--- audio/add-annotation_ID-audio.py
+++ audio/add-annotation_ID-audio.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import re
 import uuid
 import pyclan as pc
@@ -23,10 +24,10 @@ def process_file(in_file, out_file):
 		new_line = l.line
 		if len(matches)>0: 	# if there is an annotation on the line
 			for match in matches: 	# for each annotation
-				print(match, len(match))
+				# print(match, len(match))
 				if match[-1]=='' and match[-2]=='':	# if there is no id for this annot
 					# replace match by its ID-ed version
-					print("adding")
+					# print("adding")
 					# print(''.join(match))
 					# print(re.sub(''.join(match), ''.join(match)+'_'+randomID(), new_line))
 					new_line = new_line.replace(''.join(match), ''.join(match)+'_'+randomID())
@@ -42,11 +43,15 @@ def process_file(in_file, out_file):
 if __name__ == "__main__":
 
 	input = sys.argv[1]
+	if platform.system() != "Windows":
+		usedID_file = "/Volumes/pn-opus/Seedlings/usedID.txt"
+	else:
+		usedID_file = "Z:\\Seedlings\\usedID.txt"
 
 	if not input.endswith(".cha"):
 		input_dir = input
 		output_dir = sys.argv[2]
-		usedIDFile = sys.argv[3]
+		usedID_file = sys.argv[3]
 
 		files = os.listdir(input_dir)
 		files.sort()
@@ -54,7 +59,7 @@ if __name__ == "__main__":
 		counter = 0
 		usedID = Set([])
 		#Load used IDs to prevent collision
-		with open(usedIDFile) as f:
+		with open(usedID_file) as f:
 			for line in f.readlines():
 				usedID.add(line.rstrip())
 
@@ -76,7 +81,7 @@ if __name__ == "__main__":
 			counter += 1
 			print("Finished: {}".format(counter/float(len(files))*100))
 
-		with open(usedIDFile, 'w') as f: # not 'a' because not adding (or will introduce duplicates, and size of file+++)
+		with open(usedID_file, 'w') as f: # not 'a' because not adding (or will introduce duplicates, and size of file+++)
 			for id in usedID:
 				f.write(id + '\n')
 
@@ -91,18 +96,18 @@ if __name__ == "__main__":
 			out_file = in_file
 		# retrieve used id
 		usedID = Set([])
-		with open("used.txt") as f:
+		with open(usedID_file) as f:
 			for line in f.readlines():
 				usedID.add(line.rstrip())
 
 		# process file
 		try:
-			process_file(in_file, out_file, usedID)
+			process_file(in_file, out_file)
 		except Exception,e:
 			print(file)
 			print(e)
 
 		# update used id
-		with open("used.txt", 'a') as f:
+		with open(usedID_file, 'w') as f:
 			for id in usedID:
 				f.write(id + '\n')
