185e8fd5018fa55652cdb1c9f4508e9cd1b636c9
Estelle He <wh103@duke.edu>
2019-04-10 16:07:03 -0400

    check_annotid

--- audio/check_annotid.py
+++ audio/check_annotid.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import re
+import csv
 
 if __name__ == "__main__":
 
@@ -8,14 +9,22 @@ if __name__ == "__main__":
     cha_folder = sys.argv[1]
 
     error = []
-    pattern = '.*&=[^\s]*0x[0-9a-z]{6}.*'
-    print bool(re.match(pattern, '&= 0x5fdc48 .'))
+    pattern = '[0-9a-zA-Z+]+\s&=[^\s]*0x[0-9a-z]{6}'
+    pattern_word = '[0-9a-zA-Z+]+\s&=[^\s]+'
     for dirpath, dirnames, filenames in os.walk(cha_folder):
         for fname in filenames:
             if fname.endswith('.cha'):
-                with open(opf_paths, 'r') as f:
+                with open(os.path.join(cha_folder, fname), 'r') as f:
                     lines = f.readlines()
                 for line in lines:
-                    if '&=' in line:
-                        if not re.match(pattern, line):
-                            error.append([fname, line]) # timestamp and word
+                    match = re.findall(pattern_word, line)
+                    if len(match):
+                        for m in match:
+                            if not re.match(pattern, m):
+                                error.append([os.path.join(cha_folder, fname), m, line.split(' ')[-1].strip()])
+
+    with open(os.path.join(cha_folder, "error_summary.csv"), 'w') as csvf:
+        writer = csv.writer(csvf)
+        writer.writerow(["file name", "line", "timestamp"])
+        for e in error:
+            writer.writerow(e)
